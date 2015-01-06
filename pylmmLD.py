94afa8e3b3eec591e87ffefcfa6b1cc915bef714
Yue Wu <wuyue071@login1.(none)>
2015-01-05 17:40:44 -0800

    fill all missing data; faster filling

--- pylmmLD.py
+++ pylmmLD.py
@@ -24,8 +24,8 @@ basicGroup.add_option("--emmaSNP", dest="emmaFile", default=None,
 			help="\"EMMA\" file formats. This is just a text file with individuals on the rows and snps on the coumns.")
 basicGroup.add_option("--emmaNumSNPs", dest="numSNPs", type="int", default=0,
 			help="When providing the emmaSNP file you need to specify how many snps are in the file")
-basicGroup.add_option("--candidateNum", dest="candidateNums", type="int",default=100,help="You can specify the number of candidate snp to choose from to pair with a specific snp")
-
+basicGroup.add_option("--neighborNum", dest="neighborNums", type="int",default=100,help="You can specify the number of neighbor snp to choose from to pair with a specific snp")
+basicGroup.add_option("--candidateNum",dest="candidateNums",type="int", default=10, help="You can specify the number of candidate snp to look at to impute")
 
 basicGroup.add_option("-v", "--verbose", action="store_true", dest="verbose",
 			default=False, help="print extra info")
@@ -65,7 +65,10 @@ W = np.ones((numSNP, m))*np.nan
 all_snps=[]
 all_ids=[]
 count=0
-imputation = np.ones((numSNP,2))*np.nan
+imputation = np.ones((numSNP,options.candidateNums))*np.nan
+imputation_index=np.ones((numSNP,options.candidateNums))*np.nan
+
+
 for snp,id  in IN:
 	count +=1
 	if options.verbose and count % 1000 ==0:
@@ -77,32 +80,90 @@ if(options.verbose):
 	sys.stderr.write("start to pick candidate...")
 
 
-for i in range(0,numSNP, options.candidateNums/2):
-	temp = correlation(all_snps[i:min(numSNP,i+options.candidateNums)])
+optimal_imputation=[[] for i in range(numSNP)]
+optimal_correlation=[[] for i in range(numSNP)]
+optimal_index=[[] for i in range(numSNP)]
+for i in range(0,numSNP,options.neighborNums/2):
+	temp = correlation(all_snps[i:min(numSNP,i+options.neighborNums)])
 	for j in range (len(temp)):
 		temp[j][j]=0
-		max_cor= max(abs(temp[j,]))
-		a=list(abs(temp[j,]))
-		index=a.index(max_cor)
-		if np.isnan(imputation[i+j][0]):
-			imputation[i+j][0]=temp[j][index]
-			imputation[i+j][1]=i+index
-		elif max_cor > abs(imputation[i+j][0]):
-			imputation[i+j][0]=temp[j][index]
-			imputation[i+j][1]=i+index
-		else: continue
-	if (i+len(temp) >= numSNP):
-		break
-count=0
+		if not len(optimal_imputation[i+j]):
+			optimal_imputation[i+j] = list(temp[j])
+		else :
+			arr= list(temp[j][options.neighborNums/2:])
+			optimal_imputation[i+j].extend(arr)
+	if (i + len(temp)>= numSNP): break
+counter=0
 for i in range(numSNP):
-	count +=1
-	if options.verbose and count%1000 ==0:
-		sys.stderr.write("Imputation at SNP %d\n" % count)
-	for j in range(indiNum):
-		if np.isnan(all_snps[i][j]):	
-			snp_index=int(imputation[i][1])
-			all_snps[i][j]=imputation[i][0]* all_snps[snp_index][j]
-all_snps=np.array(all_snps)
+	counter+=1
+	if(options.verbose) and counter % 1000 ==0:
+		sys.stderr.write("At SNP %d\n" % counter)	
+	x = np.isnan(all_snps[i])
+	if not len(all_snps[i][x]): continue
+	if i < options.neighborNums:
+		offset=0
+	else: opffset= (i % options.neighborNums) * options.neighborNums
+	count=0
+	while len(all_snps[i][x]) and count < options.candidateNums:	
+		max_cor = max(abs(np.array(optimal_imputation[i])))	
+		a = list(abs(np.array(optimal_imputation[i])))
+		index=a.index(max_cor)
+		snp = index+offset
+		all_snps[i][x] = optimal_imputation[i][index] * all_snps[snp][x]
+		optimal_imputation[i][index]=0
+		x = np.isnan(all_snps[i])
+		count +=1
+	x = np.isnan(all_snps[i])
+	if  not len(all_snps[i]) and (options.verbose):
+		sys.stderr.write("SNP %s is not replaced\n" % all_ids[i])
+#for i in range(0,numSNP, options.neighborNums/2):
+#	temp = correlation(all_snps[i:min(numSNP,i+options.neighborNums)])
+#	for j in range (len(temp)):
+#		temp[j][j]=0
+#		counter=0
+#		a=sort(abs(temp[j,]))[(len(temp)-options.candidateNums):]
+#		a=list(a)
+#		a.reverse()
+#		counter=0
+#		b=list(abs(temp[j,]))
+#		for k in range(options.candidateNums):
+#			index=b.index(a[counter])
+#			if np.isnan(imputation[i+j][k]):
+#				imputation[i+j][k]=temp[j][index]
+#				imputation_index[i+j][k]=i+index
+#				counter +=1
+#				b[index]=0
+#			elif abs(temp[j][index])> abs(imputation[i+j][k]):
+#				imputation[i+j][k+1 :] = imputation[i+j][k:options.candidateNums-1]
+#				imputation_index[i+j][k+1:] =imputation_index[i+j][k:options.candidateNums-1]
+#
+#				imputation[i+j]=temp[j][index]
+#				imputation_index[i+j][k]=i+index
+#				counter +=1
+#				b[index]=0
+#			else: continue
+#		
+				
+#	if (i+len(temp) >= numSNP):
+#		break
+#count=0
+#non_imputed=[]
+#for i in range(numSNP):
+#	count +=1
+#	if options.verbose and count%1000 ==0:
+#		sys.stderr.write("Imputation at SNP %d\n" % count)
+#	x=np.isnan(all_snps[i])
+#	counter=0
+#	while len(all_snps[i][x]) and counter < options.candidateNums:
+#		index=int(imputation_index[i][counter])
+#		all_snps[i][x] = imputation[i][counter] * all_snps[index][x]
+#		x=np.isnan(all_snps[i])
+#		counter +=1
+#	x=np.isnan(all_snps[i])
+#	if len(all_snps[i][x]):
+#		non_imputed.append(all_ids[i])
+#		sys.stderr.write("snp %s is not replaced \n" % all_ids[i])
+#all_snps=np.array(all_snps)
 if(options.verbose):
 	sys.stderr.write("Imputed file saved as %s type\n" % all_snps.dtype)
 np.savetxt(outFile, all_snps)
