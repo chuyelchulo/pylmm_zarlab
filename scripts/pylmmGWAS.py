38395e9bf1eef6aeab0b309a0f66b876fde9530b
Nick Crawford <ngcrawford@gmail.com>
2015-07-24 10:19:18 -0400

    Added in new code

--- scripts/pylmmGWAS.py
+++ scripts/pylmmGWAS.py
@@ -43,11 +43,7 @@ def outputResult(id,beta,betaSD,ts,ps):
    out.write("\t".join([str(x) for x in [id,beta,betaSD,ts,ps]]) + "\n")
 
 
-from optparse import OptionParser,OptionGroup
-usage = """usage: %prog [options] --kfile kinshipFile --[tfile | bfile] plinkFileBase outfileBase
 
-This program provides basic genome-wide association (GWAS) functionality.  You provide a phenotype and genotype file as well as a pre-computed (use pylmmKinship.py) kinship matrix and the program outputs a result file with information about each SNP, including the association p-value.  
-The input file are all standard plink formatted with the first two columns specifiying the individual and family ID.  For the phenotype file, we accept either NA or -9 to denote missing values.  
 
 
 usage = """usage: %prog [options] --kfile kinshipFile --[tfile | bfile] plinkFileBase outfileBase
@@ -63,12 +59,6 @@ usage = """usage: %prog [options] --kfile kinshipFile --[tfile | bfile] plinkFil
 
             Basic usage:
 
-               python pylmmGWAS.py \\
-                  -v \\
-                  --bfile plinkFile \\
-                  --kfile preComputedKinship.kin \\
-                  --phenofile plinkFormattedPhenotypeFile \\
-                  resultFile
        """
 parser = OptionParser(usage=usage, 
                       option_class=MultipleOption)
@@ -222,9 +212,6 @@ if options.emmaPheno:
       v = line.strip().split()
       p = []
       for x in v:
-	 try:
-	    p.append(float(x))
-	 except: p.append(np.nan)
       P.append(p)
    f.close()
    IN.phenos = np.array(P).T
@@ -233,11 +220,8 @@ if options.emmaPheno:
 if options.covfile: 
    if options.verbose: 
       sys.stderr.write("Reading covariate file...\n")
-   P = IN.getCovariates(options.covfile) 
    if options.noMean: 
-      X0 = P
    else: 
-      X0 = np.hstack([np.ones((IN.phenos.shape[0],1)),P])
 elif options.emmaCov:
    if options.verbose: 
       sys.stderr.write("Reading covariate file...\n")
@@ -249,15 +233,12 @@ elif options.emmaCov:
 else: 
    X0 = np.ones((IN.phenos.shape[0],1))
 
-if np.isnan(X0).sum(): 
-      parser.error("The covariate file %s contains missing values. At this time we are not dealing with this case.  Either remove those individuals with missing values or replace them in some way.")
 
 # READING Kinship - major bottleneck for large datasets
 if options.verbose: sys.stderr.write("Reading kinship...\n")
 begin = time.time()
 # This method seems to be the fastest and works if you already know the size of the matrix
 if options.kfile[-3:] == '.gz':
-   import gzip
    f = gzip.open(options.kfile,'r')
    F = f.read() # might exhaust mem if the file is huge
    K = np.fromstring(F,sep=' ') # Assume that space separated
@@ -276,7 +257,6 @@ if options.kfile2:
    begin = time.time()
    # This method seems to be the fastest and works if you already know the size of the matrix
    if options.kfile2[-3:] == '.gz':
-      import gzip
       f = gzip.open(options.kfile2,'r')
       F = f.read() # might exhaust mem if the file is huge
       K2 = np.fromstring(F,sep=' ') # Assume that space separated
@@ -287,53 +267,27 @@ if options.kfile2:
    if options.verbose: sys.stderr.write("Read the %d x %d second kinship matrix in %0.3fs \n" % (K2.shape[0],K2.shape[1],end-begin))
 
 # PROCESS the phenotype data -- Remove missing phenotype values
-# Keep will now index into the "full" data to select what we keep (either everything or a subset of non missing data
-Y = IN.phenos[:,options.pheno]
-v = np.isnan(Y)
-keep = True - v
-if v.sum():
-   if options.verbose: sys.stderr.write("Cleaning the phenotype vector by removing %d individuals...\n" % (v.sum()))
-   Y = Y[keep]
-   X0 = X0[keep,:]
-   K = K[keep,:][:,keep]
-   if options.kfile2: K2 = K2[keep,:][:,keep]
-   Kva = []
-   Kve = []
+
+
 
 # Only load the decomposition if we did not remove individuals.
 # Otherwise it would not be correct and we would have to compute it again.
-if not v.sum() and options.eigenfile:
-   if options.verbose: sys.stderr.write("Loading pre-computed eigendecomposition...\n")
-   Kva = np.load(options.eigenfile + ".Kva")
-   Kve = np.load(options.eigenfile + ".Kve")
 else: 
    Kva = []
    Kve = []
 
 # CREATE LMM object for association
 n = K.shape[0]
-if not options.kfile2:  L = LMM(Y,K,Kva,Kve,X0,verbose=options.verbose)
-else:  L = LMM_withK2(Y,K,Kva,Kve,X0,verbose=options.verbose,K2=K2)
 
 # Fit the null model -- if refit is true we will refit for each SNP, so no reason to run here
-if not options.refit: 
    if options.verbose: sys.stderr.write("Computing fit for null model\n")
    L.fit()
    if options.verbose and not options.kfile2: sys.stderr.write("\t heritability=%0.3f, sigma=%0.3f\n" % (L.optH,L.optSigma))
    if options.verbose and options.kfile2: sys.stderr.write("\t heritability=%0.3f, sigma=%0.3f, w=%0.3f\n" % (L.optH,L.optSigma,L.optW))
 
 # Buffers for pvalues and t-stats
-PS = []
-TS = []
-count = 0
 out = open(outFile,'w')
-printOutHead()
 
-for snp,id in IN:
-   count += 1
-   if options.verbose and count % 1000 == 0: 
-      sys.stderr.write("At SNP %d\n" % count)
-      
    x = snp[keep].reshape((n,1))
    v = np.isnan(x).reshape((-1,))
    # Check SNPs for missing values
@@ -341,10 +295,6 @@ for snp,id in IN:
       keeps = True - v
       xs = x[keeps,:]
       if keeps.sum() <= 1 or xs.var() <= 1e-6: 
-	 PS.append(np.nan)
-	 TS.append(np.nan)
-	 outputResult(id,np.nan,np.nan,np.nan,np.nan)
-	 continue
 
       # Its ok to center the genotype -  I used options.normalizeGenotype to 
       # force the removal of missing genotypes as opposed to replacing them with MAF.
@@ -352,26 +302,9 @@ for snp,id in IN:
       Ys = Y[keeps]
       X0s = X0[keeps,:]
       Ks = K[keeps,:][:,keeps]
-      if options.kfile2: K2s = K2[keeps,:][:,keeps]
-      if options.kfile2: Ls = LMM_withK2(Ys,Ks,X0=X0s,verbose=options.verbose,K2=K2s)
-      else: Ls = LMM(Ys,Ks,X0=X0s,verbose=options.verbose)
-      if options.refit: Ls.fit(X=xs,REML=options.REML)
       else: 
-	 #try:
-	 Ls.fit(REML=options.REML)
-	 #except: pdb.set_trace()
       ts,ps,beta,betaVar = Ls.association(xs,REML=options.REML,returnBeta=True)
    else: 
       if x.var() == 0: 
-	 PS.append(np.nan)
-	 TS.append(np.nan)
-	 outputResult(id,np.nan,np.nan,np.nan,np.nan)
-	 continue
 
-      if options.refit: L.fit(X=x,REML=options.REML)
-      ts,ps,beta,betaVar = L.association(x,REML=options.REML,returnBeta=True)
-	    
-   outputResult(id,beta,np.sqrt(betaVar).sum(),ts,ps)
-   PS.append(ps)
-   TS.append(ts)
 
